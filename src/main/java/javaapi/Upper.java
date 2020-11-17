65392955b07d4754440fb6cb38692a8c34a6a99d
hex-twentyfive <czinky.gyorgy@gmail.com>
2020-11-18 00:51:45 +0100

    9th Commit

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,12 @@
+package javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+
+        String helloWorld = "Hello World!";
+
+        System.out.println(helloWorld.toUpperCase());       // V1
+
+        System.out.println("Hello World!".toUpperCase());       // V2
+    }
+}
