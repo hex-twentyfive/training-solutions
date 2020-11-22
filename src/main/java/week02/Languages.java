46783983af68584304fcd2092dc87305288f9054
hex-twentyfive <czinky.gyorgy@gmail.com>
2020-11-22 17:17:56 +0100

    Week 02 Commit

--- src/main/java/week02/Languages.java
+++ src/main/java/week02/Languages.java
@@ -1,20 +1,27 @@
 package week02;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Languages {
-    public static void main(String[] args) {
-        List<String> nyelvek = new ArrayList<>();
-        nyelvek = Arrays.asList("Java","Python","JavaScript");
 
-        for (String item : nyelvek){
-            if(item.length()>5){
-                System.out.println(item);
+    public static void main(String[] args) {
+
+        String[] programingLanguagesArray = {"Java", "Python", "JavaScript"};
+        List<String> programingLanguagesList = Arrays.asList(programingLanguagesArray);
+
+        System.out.println("A tömb tartalma: ");
+        for (String looksArround : programingLanguagesList) {
+            System.out.print(looksArround + ", ");
+        }
+
+        System.out.println("\n\nAz öt karakternél hoszabb programozási nyelvek a következők: ");
+        for (String looksArround : programingLanguagesList) {
+            if (looksArround.length() > 4) {
+                System.out.println(looksArround);
             }
         }
-    }
 
+    }
 }
 
