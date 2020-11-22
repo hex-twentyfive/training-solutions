d7c47ee82c71fb8b4c55dec1e3f878b22391416f
hex-twentyfive <czinky.gyorgy@gmail.com>
2020-11-22 13:46:58 +0100

    Week 02 Commit

--- src/main/java/week02/InputNames.java
+++ src/main/java/week02/InputNames.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 
 public class InputNames {
 
-    int[] nevek = new int[5];
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        String[] nevek = new String[5];
-        for (int i=0; i<nevek.length;i++){
-            System.out.println("Kérem adjon meg egy nevet! ");
-            nevek[i]=scanner.nextLine();
-        }
-        for (int i=0; i< nevek.length;i++){
-            System.out.println((i+1) +". elem: " + nevek[i]);
-        }
-    }
+        System.out.println("Kérem gépeljen be egymás után 5 nevet!");
 
+        String[] nameArray = new String[5];
+
+        for ( int i = 0 ; i < nameArray.length ; i++ ) {
+            System.out.println("Név " + (i + 1) + ". : ");
+            nameArray[i] = scanner.nextLine();
+        }
+
+        System.out.print("\nA név tömb tartalma: ");
+        for ( int i = 0 ; i < nameArray.length ; i++) {
+            System.out.print(nameArray[i] + ", ");
+        }
+        System.out.println("\n");
+
+    }
 }
