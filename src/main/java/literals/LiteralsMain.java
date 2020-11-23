abdf4d30ff68188174cc78cf26f390ed44332fe1
hex-twentyfive <czinky.gyorgy@gmail.com>
2020-11-23 22:37:48 +0100

    Week 02 Commit

---
+++ src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,41 @@
+package literals;
+
+public class LiteralsMain {
+
+    public static void main(String[] args) {
+
+        String number = "12";
+
+        int a = 1;
+        int b = 2;
+
+        String number2 = "" + a + b;
+
+        System.out.println(number2);
+
+        double quotient = 3 / 4;
+        System.out.println(quotient);
+
+        quotient = 3.0 / 4;
+        System.out.println(quotient);
+
+        quotient = 3 / 4.0;
+        System.out.println(quotient);
+
+        quotient = 3 / 4d;
+        System.out.println(quotient);
+
+        long big = 3_2444_444_444L;
+
+
+        String s = "árvíztűrőtükörfúrógép";
+        System.out.println(s);
+
+        String word = "title".toUpperCase();
+        System.out.println(word);
+
+        System.out.println(Integer.toBinaryString(1));
+        System.out.println(Integer.toBinaryString(-2));
+
+    }
+}
