ac81095fa0525700d742c46c3b3b319722e70412
hex-twentyfive <czinky.gyorgy@gmail.com>
2020-11-27 23:48:43 +0100

    16th Commit - Modul 1 - Chapter 4 - lesson 5

--- src/main/java/arraylist/Books.java
+++ src/main/java/arraylist/Books.java
@@ -5,26 +5,38 @@ import java.util.List;
 
 public class Books{
 
-    private List<String> books = new ArrayList<>();
+    private List<String> Books = new ArrayList<>();
 
-    public void addTitle(String  title) {
-        books.add(title);
+
+    public void add(String inputBooks) {           //felveszi a könyveket
+        Books.add(inputBooks);
     }
 
-    public List<String> getTitles() {
-        return books;
+ /*   public List<String> findAllByPrefix(String prefix) {            //mely az összes olyan könyvet visszaadja, mely címének eleje megegyezik a paraméterként átadott szöveggel.
+
+    }*/
+
+    public  List<String> getTitles() {               //visszaadja a könyvek címeit.
+        return Books;
     }
+
 
     public static void main(String[] args) {
 
-        Books konyvLista = new Books();
+        String Book1 = "Az idő rövid története";
+        String Book2 = "A tér és az idő természete";
+        String Book3 = "A káosz létsíkja";
+        String Book4 = "Mágusok tornya";
 
-        konyvLista.addTitle("Az idő rövid története");
-        konyvLista.addTitle("A tér és az idő természete");
-        konyvLista.addTitle("A káosz létsíkja");
-        konyvLista.addTitle("Mágusok tornya");
+        Books instBooks = new Books();
 
-        System.out.println(konyvLista.getTitles());
+        instBooks.add(Book1);
+        instBooks.add(Book2);
+        instBooks.add(Book3);
+        instBooks.add(Book4);
+
+        System.out.println(instBooks.getTitles());
+
     }
 
 }
