8feaa11fb7620bde932681f5c3e2abea9814d37b
hex-twentyfive <czinky.gyorgy@gmail.com>
2020-12-07 11:57:44 +0100

    Week 06 Day 03 Commit

--- src/main/java/week06d03/WordEraser.java
+++ src/main/java/week06d03/WordEraser.java
@@ -3,24 +3,32 @@ package week06d03;
 import java.util.ArrayList;
 import java.util.List;
 
+/*      A week06d03.WordEraser osztályban
+        legyen egy eraseWord(String words, String word) metódus,
+        mely az első paraméter szöveget úgy változtatja meg, hogy a második paraméter összes előforulását kitörli belőle.
+        A words paraméterben a szavak szóközzel vannak elválasztva.
+        A metódus térjen vissza a törölni kívánt szavak nélküli Stringgel.
+        (Mellőzzük a replace és replaceAll használatát)
+        pl: worlds = "alma körte barack alma szilva körte birsalma"
+        word = "alma"
+        ==> "körte barack szilva körte birsalma"
+        TESZT!!!
+*/
+
 public class WordEraser {
 
     public String eraseWord(String words, String word) {
-        String[] splittedWords = words.split(" ");
-        List<String> list = new ArrayList<>();
 
-        for (String s : splittedWords) {
-            if (!s.equals(word)) {
-                list.add(s);
+        StringBuilder remaindWords = new StringBuilder();
+
+        String[] splittedWords = words.split(" ");
+
+        for (String elements: splittedWords) {
+            if(!elements.equals(word)) {
+                remaindWords.append(elements + " ");
             }
         }
-
-        String resultString = "";
-        for (String s : list) {
-            resultString += s + " ";
-        }
-        resultString =resultString.trim();
-        return resultString;
+        return remaindWords.toString().trim();          // mert a végén marad egy felesleges " "
     }
 
 }
