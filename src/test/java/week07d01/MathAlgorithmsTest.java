b6591cc07981e971f410efd0bc0a6e4dc411055a
hex-twentyfive <czinky.gyorgy@gmail.com>
2020-12-17 15:30:24 +0100

    Week 07 Day 01 Test Commit

--- src/test/java/week07d01/MathAlgorithmsTest.java
+++ src/test/java/week07d01/MathAlgorithmsTest.java
@@ -8,39 +8,51 @@ class MathAlgorithmsTest {
 
 
     @Test
-    public void tesztIsPrim2() {
-        int testNumber = 2;
+    public void tesztIsPrimNegativeNumber() {
+        int testNumber = -1;
+        String expectedExeption = "The value of the number cannot be negative!";
 
-        assertTrue(MathAlgorithms.isPrime(testNumber));
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> new MathAlgorithms().isPrime(testNumber));
+        assertEquals(expectedExeption, ex.getMessage());
     }
 
     @Test
-    public void tesztIsPrim1() {
+    public void tesztIsPrimeZeroNumber() {
+        int testNumber = 0;
+        String expectedExeption = "Wrong parameter!";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new MathAlgorithms().isPrime(testNumber));
+        assertEquals(expectedExeption, exception.getMessage() );
+    }
+
+    @Test
+    public void tesztIsPrimWithNumberOne() {
         int testNumber = 1;
 
         assertFalse(MathAlgorithms.isPrime(testNumber));
     }
 
     @Test
-    public void tesztIsPrim100() {
+    public void tesztIsPrimWithNumberTwo() {
+        int testNumber = 2;
+
+        assertTrue(MathAlgorithms.isPrime(testNumber));
+    }
+
+    @Test
+    public void tesztIsPrimWithOneHundred() {
         int testNumber = 100;
 
         assertFalse(MathAlgorithms.isPrime(testNumber));
     }
 
     @Test
-    public void tesztIsPrim199() {
-        int testNumber = 199;
+    public void tesztIsPrimOneHundredAndOne() {
+        int testNumber = 101;
 
         assertTrue(MathAlgorithms.isPrime(testNumber));
     }
 
-    @Test
-    public void tesztIsPrimNeg() {
-        int testNumber = -1;
 
-        Exception ex = assertThrows(IllegalArgumentException.class, () -> new MathAlgorithms().isPrime(testNumber));
-        assertEquals("The value of the number cannot be negative!", ex.getMessage());
-    }
 
 }
