3336deccfe8865c3e371f8e32af0dc4a43103772
hex-twentyfive <czinky.gyorgy@gmail.com>
2020-12-21 22:56:00 +0100

    Week 08 Day 01 Commit

---
+++ src/main/java/week08d01/Position.java
@@ -0,0 +1,33 @@
+package week08d01;
+
+public class Position {
+
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void increaseX(){
+        x++;
+    }
+    public void decreaseX(){
+        x--;
+    }
+    public void increaseY(){
+        y++;
+    }
+    public void decreaseY(){
+        y--;
+    }
+}
