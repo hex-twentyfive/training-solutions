c187398d00c17bc5640fc17d71e2f72138dcbb3d
hex-twentyfive <czinky.gyorgy@gmail.com>
2020-12-22 09:53:02 +0100

    70th Commit - Modul 2 - Chapter 4 - lesson 2

---
+++ src/main/java/interfaces/simplethread/SimpleThread.java
@@ -0,0 +1,34 @@
+package interfaces.simplethread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleThread implements Runnable {
+
+    private List<String> tasks = new ArrayList<>();
+
+    public SimpleThread(List<String> tasks) {
+        this.tasks = tasks;
+    }
+
+    public List<String> getTasks() {
+        return tasks;
+    }
+
+    private boolean nextStep() {
+
+        if (tasks.size() > 0) {
+            tasks.remove(tasks.size()-1);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void run() {
+        while (tasks.size() > 0) {
+            nextStep();
+        }
+    }
+
+}
