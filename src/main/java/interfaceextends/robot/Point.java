b47492bbad0c345599a5ff055ee6c6ff14ebfdbe
hex-twentyfive <czinky.gyorgy@gmail.com>
2020-12-28 12:38:23 +0100

    72nd Commit - Modul 2 - Chapter 4 - lesson 6

---
+++ src/main/java/interfaceextends/robot/Point.java
@@ -0,0 +1,26 @@
+package interfaceextends.robot;
+
+public class Point {
+
+    final private long X;
+    final private long Y;
+    final private long Z;
+
+    public Point(long x, long y, long z) {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+    }
+
+    public long getX() {
+        return X;
+    }
+
+    public long getY() {
+        return Y;
+    }
+
+    public long getZ() {
+        return Z;
+    }
+}
