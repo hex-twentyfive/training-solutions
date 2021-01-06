2bf86b0398c8e502d284ffc9a879b0e5edfb817b
hex-twentyfive <czinky.gyorgy@gmail.com>
2021-01-06 20:25:25 +0100

    2. Rész vizsga - gyakolati feladat megoldás - ArraySelector

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,4 +1,24 @@
 package exam02;
 
 public class ArraySelector {
+
+    public String selectEvens(int[] numbers) {
+
+        if (numbers.length == 0) {
+            return "";
+        }
+
+        StringBuilder stringBuilder = new StringBuilder("[");
+        stringBuilder.append(numbers[0]);
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (i % 2 == 0 ) {
+                stringBuilder.append(", " + numbers[i]);
+            }
+        }
+
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
 }
