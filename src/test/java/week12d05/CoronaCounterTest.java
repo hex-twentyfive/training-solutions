f3115b6cad93c340161c40020892b0e6ce5ba04e
hex-twentyfive <czinky.gyorgy@gmail.com>
2021-02-01 19:25:13 +0100

    Week 12 Day 05 Test Commit

---
+++ src/test/java/week12d05/CoronaCounterTest.java
@@ -0,0 +1,28 @@
+package week12d05;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoronaCounterTest {
+
+
+    CoronaCounter access = new CoronaCounter();
+
+    @Test
+    public void testFileReader() {
+        assertEquals(0, access.getReadFileRows().size());
+
+        access.fileReader(access.getFileLocation());
+
+        assertEquals(7990, access.getReadFileRows().size());
+    }
+
+    @Test
+    public void testCoronaCounter() {
+        String testWordToSearch = "koronavírus";
+        access.fileReader(access.getFileLocation());
+
+        assertEquals(6, access.coronaCounter(access.getReadFileRows(), testWordToSearch));
+    }
+
+}
